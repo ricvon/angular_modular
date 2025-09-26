101bcc3af8fc31588d6edfe8ca24c0ec00772aa1
Ricardo espirito santo <ricardo.santo@gmail.com>
2025-09-26 16:38:46 -0400

    Aula 80. Capturando eventos de rotas para definir o titulo e subtitulo de cada página

--- src/app/template/layout/layout.ts
+++ src/app/template/layout/layout.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutProps } from './layoutprops';
+import { ActivatedRoute, Router} from '@angular/router';
+import { filter, map } from 'rxjs'
+
 
 @Component({
   selector: 'app-layout',
@@ -6,6 +10,30 @@ import { Component } from '@angular/core';
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
-export class Layout {
+export class Layout implements OnInit{
+  props: LayoutProps={titulo: '', subTitulo:''}
 
+  constructor(
+    private router:Router,
+    private activatedRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.router.events
+    .pipe(
+      filter(() => this.activatedRoute.firstChild !== null),
+      map( () => this.obterPropriedadesLayout())
+    ).subscribe((props:LayoutProps)=>this.props=props)
+    this.props = this.obterPropriedadesLayout();
+  }
+
+  obterPropriedadesLayout():LayoutProps{
+    let rotaFilha = this.activatedRoute.firstChild;
+
+    while (rotaFilha?.firstChild){
+      rotaFilha = rotaFilha.firstChild;
+    }
+
+    return rotaFilha?.snapshot.data as LayoutProps;
+  }
 }
--- src/app/template/template-routing-module.ts
+++ src/app/template/template-routing-module.ts
@@ -12,17 +12,20 @@ const routes: Routes = [
     {
       path: 'categorias',
       loadChildren: () => import('../categorias/categorias-module').then(m=>m.CategoriasModule),
-      pathMatch:'full'
+      pathMatch:'full',
+      data: {titulo:'Categorias', subTitulo:'Realize o cadastro de novas categorias'}
     },
     {
       path: 'lugares',
       loadChildren: () => import('../lugares/lugares-module').then(m=>m.LugaresModule),
-      pathMatch:'full'
+      pathMatch:'full',
+      data: {titulo:'Lugares', subTitulo:'Realize o cadastro de novos lugares'}
     },
     {
       path: 'galeria',
       loadChildren: () => import('../galeria/galeria-module').then(m=>m.GaleriaModule),
-      pathMatch:'full'
+      pathMatch:'full',
+      data: {titulo:'Lista de indicação de lugares.', subTitulo:'Descobra os melhores lugares para visitar.'}
     }
   ]
 }
