43f881e410693efca71a47a82e391a7aaee83896
Ricardo espirito santo <ricardo.santo@gmail.com>
2025-09-26 11:11:19 -0400

    Aula 77. Implementando a pesquisa de lugares

--- src/app/galeria/galeria/galeria.component.ts
+++ src/app/galeria/galeria/galeria.component.ts
@@ -14,6 +14,9 @@ import { CategoriaService } from '../../categorias/categoria.service';
 export class GaleriaComponent implements OnInit{
   lugares: Lugar[]=[];
   categoriasFiltro: Categoria[]=[];
+  nomeFiltro:string='';
+  categoriaFiltro:string='';
+
   constructor(
     private lugarService: LugarService,
     private categoriaService: CategoriaService
@@ -29,5 +32,11 @@ export class GaleriaComponent implements OnInit{
 
   getTotalEstrelas(lugar:Lugar):string{
     return '&#9733;'.repeat(lugar.avaliacao||0)+'&#9734;'.repeat(5 - (lugar.avaliacao||0));
+  }
+
+  filtrar(){
+    //console.log("valores digitados:", this.nomeFiltro, this.categoriaFiltro);
+    this.lugarService.filtrar(this.nomeFiltro, this.categoriaFiltro)
+    .subscribe(resultado => this.lugares=resultado);
   }
 }
--- src/app/lugares/lugar.service.ts
+++ src/app/lugares/lugar.service.ts
@@ -1,19 +1,32 @@
 import { Lugar } from './lugar/lugar';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LugarService {
-    constructor(private http: HttpClient){ }
-
+  constructor(private http: HttpClient){ }
       salvar(lugar: Lugar) : Observable<Lugar>{
-        return this.http.post<Lugar>('http://localhost:3000/lugares', lugar);
-      }
+      return this.http.post<Lugar>('http://localhost:3000/lugares', lugar);
+  }
 
   ObterTodos():Observable<Lugar[]>{
     return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+  }
+
+  filtrar (nome: string, categoria: string):Observable<Lugar[]>{
+    let parametros = new HttpParams();
+    if (nome){
+      parametros = parametros.set('nome_like', nome)
+    }
+    if (categoria){
+      parametros = parametros.set('categoria', categoria);
+    }
+    //console.log("valores digitados service:", parametros);
+    return this.http.get<Lugar[]>('http://localhost:3000/lugares', {
+      params:parametros
+    });
   }
 }
